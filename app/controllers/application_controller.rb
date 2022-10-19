58676fceb790c9e19f04b83e80bda086bdeca3b9
bdc7ba <bcrozier@chicagobooth.edu>
2022-10-19 16:00:05 +0000

    progress on paper and wrapper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
     # redirect_to("https://www.wikipedia.org")
     # render({ :html => "<h1>Hello, world!</h1>".html_safe})
-    render({ :template => "game_templates/user_rock.html.erb"})
+    render({ :template => "game_templates/user_rock.html.erb", :layout => "wrapper.html.erb"})
   end
 
   def play_paper 
